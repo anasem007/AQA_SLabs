a11f42be6e9bf6fe75ebb4ef5a634d5492b8b426
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-19 10:03:58 +0300

    Add assertions

---
+++ TestRailApi/Asserts/ProjectAssertions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using TestRailApi.Models.Project;
+
+namespace TestRailApi.Asserts
+{
+    public static class ProjectAssertions
+    {
+        public static void ShouldBeValid(this ProjectResponseModel responseModel, ProjectRequestModel requestModel)
+        {
+            using (new AssertionScope())
+            {
+                responseModel.Should().BeEquivalentTo(requestModel);
+                responseModel.CompletedOn.Should().BeNull();
+                responseModel.IsCompleted.Should().BeFalse();
+                Uri.IsWellFormedUriString(responseModel.Url, UriKind.Absolute).Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ TestRailApi/Asserts/SuiteAssertions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using TestRailApi.Models.Suite;
+
+namespace TestRailApi.Asserts
+{
+    public static class SuiteAssertions
+    {
+        public static void ShouldBeValid(this SuiteResponseModel response, SuiteRequestModel request)
+        {
+            using (new AssertionScope())
+            {
+                response.Should().BeEquivalentTo(request);
+                response.IsMaster.Should().BeFalse();
+                response.IsBaseline.Should().BeFalse();
+                response.IsCompleted.Should().BeFalse();
+                response.CompletedOn.Should().BeNull();
+                Uri.IsWellFormedUriString(response.Url, UriKind.Absolute).Should().BeTrue();
+            }
+        }
+    }
+}
