9fcdbe1ad1bfa07d498fae1ceeb5d18791102d20
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-19 10:16:56 +0300

    Add constants

---
+++ PageObject/Constants/EndPoints.cs
@@ -0,0 +1,15 @@
+namespace PageObject.Constants
+{
+    public static class EndPoints
+    {
+        public const string ApiAddProjectEndPoint = "index.php?/api/v2/add_project";
+        public const string ApiGetProjectEndPoint = "index.php?/api/v2/get_project/";
+        public const string ApiDeleteProjectEndPoint = "index.php?/api/v2/delete_project/";
+        public const string ApiAddSuiteEndPoint = "index.php?/api/v2/add_suite/";
+        public const string ApiUpdateSuiteEndPoint = "index.php?/api/v2/add_suite/";
+
+        public const string AdminAddProjectEndPoint = "index.php?/admin/projects/add/1";
+        public const string DashboardEndPoint = "index.php?/dashboard";
+        public const string LoginEndPoint = "";
+    }
+}
