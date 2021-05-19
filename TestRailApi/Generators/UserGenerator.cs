714e3ceb74ac2d9b6fea06e6f7c5f7019ba37314
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-19 09:58:28 +0300

    Add model generators

---
+++ TestRailApi/Generators/UserGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using TestRailApi.Models.User;
+
+namespace TestRailApi.Generators
+{
+    public static class UserGenerator
+    {
+        public static User GetUser(string id)
+        {
+            foreach (var user in TestData.Users.Where(user => user.Id == id))
+            {
+                return user;
+            }
+
+            throw new Exception();
+        }
+    }
+}
