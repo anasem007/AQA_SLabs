b1cde79d100755be0c4f23b07c633ceeaf69874c
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-19 09:59:20 +0300

    Add services

---
+++ TestRailApi/Services/ProjectService.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using RestSharp;
+using TestRailApi.ClientExtensions;
+using TestRailApi.Constants;
+using TestRailApi.Models.Project;
+using TestRailApi.Models.User;
+
+namespace TestRailApi.Services
+{
+    public class ProjectService
+    {
+        private readonly RestClient _client;
+
+        public ProjectService(User user)
+        {
+            _client = user.CreateClient();
+        }
+        
+        public async Task<IRestResponse<ProjectResponseModel>> AddProject(ProjectRequestModel project)
+        {
+            var request = RestRequestBuilder.Build(EndPoints.AddProjectEndPoint, Method.POST, project);
+            
+            return await _client.ExecuteAsync<ProjectResponseModel>(request);
+        }
+        
+        public async Task<IRestResponse<ProjectResponseModel>> DeleteProject(string projectId)
+        {
+            var request = RestRequestBuilder.Build(EndPoints.DeleteProjectEndPoint + projectId, Method.POST);
+            
+            return await _client.ExecuteAsync<ProjectResponseModel>(request);
+        }    
+        
+        public async Task<IRestResponse<ProjectResponseModel>> GetProject(string projectId)
+        {
+            var request = RestRequestBuilder.Build(EndPoints.GetProjectEndPoint + projectId, Method.GET);
+            
+            return await _client.ExecuteAsync<ProjectResponseModel>(request);
+        }   
+    }
+}
