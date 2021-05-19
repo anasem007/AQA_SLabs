0fd27121ddb9723d38a136d5c08b0d33c70b3ea5
Anastasiya Semenova <anastasiyasemen0.0va@gmail.com>
2021-05-19 09:36:38 +0300

    Add services

---
+++ Saucedemo/Services/ModelService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text.Json;
+using Saucedemo.Models;
+
+namespace Saucedemo.Services
+{
+    public static class ModelService
+    {
+        private const string UsersFileName = "users";
+
+        public static IEnumerable<User> Users => GenerateModels<User>(UsersFileName);
+        private static List<T> GenerateModels<T>(string name)
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var fullPathToFile = Path.Combine(basePath ?? string.Empty, $"TestData{Path.DirectorySeparatorChar}", $@"{name}.json");
+
+            var jsonSerializerOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            
+            var jsonStream = File.ReadAllText(fullPathToFile);
+            
+            return JsonSerializer.Deserialize<List<T>>(jsonStream);
+        }
+    }
+}
---
+++ Saucedemo/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using Saucedemo.Models;
+
+namespace Saucedemo.Services
+{
+    public class UserService
+    {
+        public static User GetUser(string name)
+        {
+            var neededUser = new User();
+            foreach (var user in ModelService.Users)
+            {
+                if (user.Username.Equals(name))
+                {
+                    return new User
+                    {
+                        Username = user.Username,
+                        Password = user.Password
+                    };
+                }
+            }
+
+            throw new NotFoundException($"User with such name {name} not found.");
+        }
+    }
+}
